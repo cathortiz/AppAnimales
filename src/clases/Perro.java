99a769528b2ab7f030a9042350aa24747903f80d
Catherin Ortiz <catherinortiz@gmail.com>
2017-09-06 15:43:16 -0300

    Agrego la clase perro

---
+++ src/clases/Perro.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author Admin
+ */
+public class Perro extends Animal implements Mascota{
+
+    @Override
+    public void comer(String comida) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void pelearCon(Animal contrincante) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void hacerRuido() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
