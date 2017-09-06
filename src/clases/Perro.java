fe10f42689c9291d5c6d55154ee9316cd83b318b
Catherin Ortiz <catherinortiz@gmail.com>
2017-09-06 16:08:12 -0300

    Modifico la clase perro

--- src/clases/Perro.java
+++ src/clases/Perro.java
@@ -10,20 +10,33 @@ package clases;
  * @author Admin
  */
 public class Perro extends Animal implements Mascota{
-
+    private int peso;
+    
     @Override
     public void comer(String comida) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(comida.toLowerCase().equals("purina")){
+            System.out.println("Mmmm, gracias!");
+        } else {
+            System.out.println("No gracias, no me gusta " + comida);
+        }
     }
 
     @Override
     public void pelearCon(Animal contrincante) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(this.getSexo()==contrincante.getSexo()){
+            if(this.getSexo() == Sexo.HEMBRA){
+                System.out.println("Las perras se ladran");
+            } else {
+                System.out.println("Los perros se pelean");
+            }
+        } else {
+            System.out.println("No hay pelea");
+        }
     }
 
     @Override
     public void hacerRuido() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Guau Guau");
     }
     
 }
